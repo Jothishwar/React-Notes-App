b6a814dd0d20c5ce523f7b41167c44bfc442051e
jothishwar <jothishwarvvr@gmail.com>
2022-11-04 15:16:09 +0530

    added data to localStorage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import NotesList from './components/notesList';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Search from './components/Search';
 import Header from './components/Header';
 
 function App() {
-  const [notes,setNotes] = useState([
-    {
-      id:nanoid(),
-      text:"this is 1st note",
-      date:"01/11/2022",
-    },
-    {
-      id:nanoid(),
-      text:"this is 2nd note",
-      date:"02/11/2022",
-    },
-    {
-      id:nanoid(),
-      text:"this is 3rd note",
-      date:"03/11/2022",
-    },
-    {
-      id:nanoid(),
-      text:"this is 4th note",
-      date:"04/11/2022",
-    }
-  ])
+  const [notes,setNotes] = useState(()=>{
+    const saved = JSON.parse(localStorage.getItem('react-notes-app-data'));
+    return saved || " ";
+  });
 
   const [searchText,setSearchText]=useState(''); 
 
   const [darkMode,setDarkMode]=useState(false);
+
+  // useEffect(() => {
+  //   const savedNotes = JSON.parse(
+  //     localStorage.getItem('react-notes-app-data')
+  //   );
+
+  //   if (savedNotes) {
+  //     setNotes(savedNotes);
+  //   }
+  // }, []);
+
+  useEffect(()=>{
+    localStorage.setItem(
+          'react-notes-app-data',
+          JSON.stringify(notes)
+      );
+  },[notes]);
 
   const handleSave = (text) => {
     console.log(text);
